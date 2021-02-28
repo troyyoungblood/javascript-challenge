c2d2798f977a4aa088d70139cc0e91f591e46a70
troyyoungblood <72400175+troyyoungblood@users.noreply.github.com>
2021-02-27 19:18:39 -0600

    upload static folder

---
+++ UFO_level_1/static/js/app.js
@@ -0,0 +1,65 @@
+// from data.js
+let tableData = data;
+
+let tbody = d3.select("tbody");
+
+tableData.forEach((dateRequest) => {
+  let row = tbody.append("tr");
+  Object.entries(dateRequest).forEach(([key, value]) => {
+    let cell = row.append("td");
+    cell.text(value);
+  });
+});
+
+// // confirming table read into app correctly
+// console.log(tableData);
+
+// Select the button
+let button = d3.select("#filter-btn");
+
+// Select the form
+// let form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+// form.on("submit",runEnter);
+
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // resets the table
+  let tbody = d3.select("tbody");
+  tbody.html("")
+
+  // Select the input element and get the raw HTML node
+  // this is the id associated input form -control
+  let inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  let inputDate = inputElement.property("value");
+
+  console.log(inputDate);
+  // console.log(tableData);
+
+  let filteredData = tableData.filter(dateEvent => dateEvent.datetime === inputDate);
+
+  console.log(filteredData);
+
+  // // Get a reference to the table body
+  // let tbody = d3.select("tbody");
+
+  filteredData.forEach((dateRequest) => {
+    let row = tbody.append("tr");
+    Object.entries(dateRequest).forEach(([key, value]) => {
+      let cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+
+
+
+  
+
+}
